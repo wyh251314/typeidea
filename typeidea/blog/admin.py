84347b1f7065401797aef15a1ef77c3622353b69
wyh <2514145727@qq.com>
2019-07-26 18:08:37 +0800

    add something

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -2,7 +2,34 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
+from .adminforms import PostAdminForm
 from .models import Post, Category, Tag
+from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
+
+class PostInline(admin.TabularInline):
+
+    fields = ('title', 'desc')
+    extra = 1 # 控制额外多几个
+    model = Post
+
+@admin.register(Category)
+class CategoryAdmin(BaseOwnerAdmin):
+    inlines = [PostInline, ]
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    # list_display = [CategoryOwnerFilter] # 让用户在侧边栏的过滤器只看到自己创建的分类
+    fields = ('name', 'status', 'is_nav')
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = "文章数量"
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
 
 class CategoryOwnerFilter(admin.SimpleListFilter):
     """ 自定义过滤器只展示当前分类 """
@@ -20,35 +47,18 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
         return queryset
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    # list_display = ('name', 'status', 'is_nav', 'created_time')
-    list_display = [CategoryOwnerFilter] # 让用户在侧边栏的过滤器只看到自己创建的分类
-    fields = ('name', 'status', 'is_nav')
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'created_time')
-    fields = ('name', 'status')
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
-
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     list_display = [
         'title', 'category', 'status',
         'created_time', 'operator'
     ]
     list_display_links = []
 
-    list_filter = ['category']
+    list_filter = [CategoryOwnerFilter, ]
     search_fields = ['title', 'category_name']
+
 
     actions_on_top = True
     actions_on_bottom = True
@@ -56,16 +66,17 @@ class PostAdmin(admin.ModelAdmin):
     # 编写页面
     save_on_top = True
 
+
     exclude = ('owner',)
-
-    # fields = (
-    #     ('category', 'title'),
-    #     'desc',
-    #     'status',
-    #     'content',
-    #     'tag',
-    # )
-
+    """
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+    """
     fieldsets = (
         ('基础配置', {
             'description': '基础配置描述',
@@ -81,25 +92,19 @@ class PostAdmin(admin.ModelAdmin):
             ),
         }),
         ('额外信息', {
-            'classes': ('collapse',),
+            'classes': ('wide',),
             'fields': ('tag',),
         })
     )
+    # filter_horizontal = ('tag', )
+    filter_vertical = ('tag', )
 
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
     operator.short_description = "操作"
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
-
-    def get_queryset(self, request):
-        qs = super(PostAdmin, self).get_queryset(request)
-        return qs.filter(owner=request.user)
 
     class Media:
         css = {
